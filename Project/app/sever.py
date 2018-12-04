341b3caccb8736c905487fa38494a34ceab2a42d
Fontaine_Lab <fontaine_gzy@163.com>
2018-12-04 14:50:58 +0800

    delete charts package

--- Project/app/sever.py
+++ Project/app/sever.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask import render_template
-import app.charts as charts
-
+from app.inhousetools import countryInfo
 from . import app
+
 
 
 
 
 @app.route("/")
 def hello():
+    # print(countryInfo.get_country_location('US'))
     return render_template('index.html')
 
