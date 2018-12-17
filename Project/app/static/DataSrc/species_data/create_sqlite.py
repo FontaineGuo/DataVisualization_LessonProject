ff418e987e4a1a216e6d0919e7016f8261ec7f34
Fontaine_Lab <fontaine_gzy@163.com>
2018-12-17 16:34:10 +0800

    import cities listings to sql

---
+++ Project/app/static/DataSrc/species_data/create_sqlite.py
@@ -0,0 +1,7 @@
+import pandas
+import csv, sqlite3
+conn= sqlite3.connect("./cites_listings_game.db")
+listData = pandas.read_csv('./cites_listings_short.csv')
+listData.to_sql('cites_listings', conn, if_exists='append', index=False)
+
+print('ok')
