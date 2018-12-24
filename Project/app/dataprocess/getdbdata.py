125bb544a12897af121f74e8147713477db9d8cb
Fontaine_Lab <fontaine_gzy@163.com>
2018-12-24 17:46:57 +0800

    edit getdbdata

--- Project/app/dataprocess/getdbdata.py
+++ Project/app/dataprocess/getdbdata.py
@@ -1,2 +1,46 @@
-
+import os
 current_path = os.path.dirname(__file__)
+import sqlite3
+
+def get_country_data(code):
+    conn = sqlite3.connect(current_path + '\\DataSrc\\countryCode_data\\country_info.db')
+    cursor = conn.cursor()
+    geodata = []
+    cursor.execute("SELECT * from geoinfo where geoinfo.Alpha2code='" + code +"'" )
+    data = cursor.fetchall()
+    for item in data:
+        print(item)
+    cursor.execute("SELECT * from nameinfo where nameinfo.code='" + code + "'")
+    data = cursor.fetchall()
+    for item in data:
+        print(item)
+    cursor.close()
+    conn.close()
+
+
+def get_cites_list_data(sciName):
+    conn = sqlite3.connect(current_path + '\\DataSrc\\species_data\\cites_listings.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * from cites_listings where cites_listings.ScientificName='" + sciName + "'")
+    data = cursor.fetchall()
+    for item in data:
+        print(item)
+    cursor.close()
+    conn.close()
+
+def get_import_src_country_list(countryCode):
+    conn = sqlite3.connect(current_path + '\\DataSrc\\trade_data\\CitesTradeData.db')
+
+    cursor = conn.execute("SELECT Exporter, count(Exporter) from CitesTradeData where CitesTradeData.Importer='"+ countryCode + "'group by Exporter" )
+    for item in cursor:
+        print(item)
+    cursor.close()
+    conn.close()
+# test for get_country_data
+# get_country_data('AL')
+
+# test for get_cites_list_data
+# get_cites_list_data('Bos gaurus')
+
+# test for get_import_src_country_list()
+get_import_src_country_list('US')
