9df78576bfae46eceaaec4b26f08ba60786e3ed7
Fontaine_Lab <fontaine_gzy@163.com>
2018-12-24 15:39:32 +0800

    create citie_listings db

--- Project/app/static/DataSrc/species_data/create_sqlite.py
+++ Project/app/static/DataSrc/species_data/create_sqlite.py
@@ -1,6 +1,6 @@
 import pandas
 import csv, sqlite3
-conn= sqlite3.connect("./cites_listings_game.db")
+conn= sqlite3.connect("./cites_listings.db")
 listData = pandas.read_csv('./cites_listings_short.csv')
 listData.to_sql('cites_listings', conn, if_exists='append', index=False)
 
